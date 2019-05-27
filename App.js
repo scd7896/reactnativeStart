c754ced97e38760de50caa5b0980326219bdb880
kimserver <scd1212@naver.com>
2019-05-27 20:29:20 +0900

    리액트네이티브 1day

---
+++ App.js
@@ -0,0 +1,56 @@
+import React , {Component} from 'react';
+import { StyleSheet, Text, View,ActivityIndicator } from 'react-native';
+import {Button}from 'react-native'
+export default class App extends Component {
+  state = {
+    isLoad : false,
+    whetherData : ''
+  }
+  render() {
+    const {isLoad} = this.state;
+    return (
+      <View style={styles.container}>
+        {isLoad ? null : 
+            <View style = {styles.loading}>
+              <Text style ={styles.loadingText}>날씨 정보를 받아오고있자너</Text>
+            </View>}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor : '#fff', //백그라운드
+    //flexDirection : 'row' 컴포넌트들의 배정을 위치를 가로로 할것인지 세로로 할 것인지 결정
+    // justifyContent :'space-around', // 세로줄의 중앙에 배치
+    // alignItems: 'stretch', // 가로줄로 중앙에 배치
+    // flexDirection : 'row',
+    // flexWrap : 'wrap'
+  },
+  // redView: {
+  //   height: 50,
+  //   width : 50,
+  //   backgroundColor: 'red',
+  //   //alignSelf : 'flex-start' 세로로 자신의 위치를 정할 수 있다.
+
+  // },
+  // yellowView: {
+  //   height : 50,
+  //   width : 50,
+  //   backgroundColor: 'yellow',
+  //   //alignSelf : 'center'
+  // }
+  loading : {
+    flex : 1,
+    backgroundColor : '#FDF6AA',
+    justifyContent : 'flex-end',
+    paddingLeft : 25
+  },
+  loadingText : {
+    fontSize : 25,
+    marginBottom : 100,
+  }
+});
+
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # reactnativeStart
+
+
+## 리액트 네이티브를 시작했습니다. 
